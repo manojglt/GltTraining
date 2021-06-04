94ec5e2a3cba96e1ac6e59c44d9bae9e3c2e34dd
manojglt <manojyekolla94@gmail.com>
2021-06-04 10:02:28 +0530

    snapshot

---
+++ snapshot/app/first/first.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+
+  trainer : string;
+  constructor(private _router : Router) { 
+   this.trainer="Prasanna";
+  }
+
+  third() {
+    this._router.navigate(['/third/',this.trainer]);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
